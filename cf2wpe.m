b440479001f9e1be2a2efacebb8107da8218c88b
Lt Cdr Gavin Edwards <gavine@sentientvision.com>
2021-08-21 16:05:51 -0700

    Fixed the switched lat and long fields and changed the order so that the start waypoint (0) is appended to the end of the list of waypoints.  This keeps the waypoints and the kneeboards in the same order.

--- cf2wpe.m
+++ cf2wpe.m
@@ -4,14 +4,19 @@ clear; pkg load io; format long;
 
 input_file = input("Input filename:\n", "s");
 
-if input_file != 2
-  printf ("Not a valid file\n");
-  printf ("Assuming default filename\n");
-  input_file = "navaids.dat";
+if exist (input_file) != 2;
+  if exist ("navaids.dat") == 2;
+    printf ("Invalid file, instad using defaul navaids.dat\n");
+    input_file = "navaids.dat";
+  else
+  printf ("No vaild file available, copy navaids.dat to this directory or check the filename spelling.\n\n");
+  return;
+  endif
 endif
 
-coords = dlmread (input_file, ";", "B2:C78" );
-finger = strtrunc (hash ("md5", mat2str (coords)), 4);
+coords = dlmread (input_file, ";", "B3:C78" );
+starting = dlmread (input_file, ";", "B2:C2" );
+finger = strtrunc (hash ("md5", mat2str ([coords; starting])), 4);
 
 # Create waypoint objects.
 
@@ -25,9 +30,17 @@ for i = 1:n
   wpe.waypoints(i).name = i;
   #wpe.waypoints(i).sequence = "0";
   wpe.waypoints(i).wp_type = "WP";
-  wpe.waypoints(i).latitude = coords(i,1);
-  wpe.waypoints(i).longitude = coords(i,2);
+  wpe.waypoints(i).latitude = coords(i,2);
+  wpe.waypoints(i).longitude = coords(i,1);
 endfor
+
+wpe.waypoints(n+1).number = n+1;
+wpe.waypoints(n+1).elevation = 0;
+wpe.waypoints(n+1).name = "START";
+#wpe.waypoints(n+1).sequence = "0";
+wpe.waypoints(n+1).wp_type = "WP";
+wpe.waypoints(n+1).latitude = starting(1,2);
+wpe.waypoints(n+1).longitude = starting(1,1);
 
 wpe.name = strcat ("cf2wpe-", datestr(date(),"yyyymmdd"), "-", finger);
 wpe.aircraft = "harrier";
